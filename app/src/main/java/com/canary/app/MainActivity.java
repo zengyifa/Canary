a0a33d517a6ea268c999e6deaf1b8b7c82865f5b
Steven Zeng <zengyifa@umich.edu>
2014-09-06 01:06:33 -0400

    sms stuff

--- app/src/main/java/com/canary/app/MainActivity.java
+++ app/src/main/java/com/canary/app/MainActivity.java
@@ -4,16 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.app.PendingIntent;
+import android.telephony.gsm.SmsManager;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+            super.onCreate(savedInstanceState);
+        sendSMS("7852182716", "hello test");
         setContentView(R.layout.activity_main);
     }
-
+    private void sendSMS(String phoneNumber, String message)
+    {
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, pi, null);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
