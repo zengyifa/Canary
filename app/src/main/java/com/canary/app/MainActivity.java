7842d52ca649080f07761ed97713c3acbdf386e3
Steven Zeng <zengyifa@umich.edu>
2014-09-06 01:34:12 -0400

    ontouchlistener

--- app/src/main/java/com/canary/app/MainActivity.java
+++ app/src/main/java/com/canary/app/MainActivity.java
@@ -7,14 +7,27 @@ import android.view.MenuItem;
 import android.app.PendingIntent;
 import android.telephony.gsm.SmsManager;
 import android.content.Intent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-        sendSMS("7852182716", "hello test");
         setContentView(R.layout.activity_main);
+         RelativeLayout Rl;
+        Rl = (RelativeLayout)findViewById(R.id.main);
+        Rl.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                sendSMS("7852182716", "hello test");
+                return true;
+            }
+        });
+
+
     }
     private void sendSMS(String phoneNumber, String message)
     {
